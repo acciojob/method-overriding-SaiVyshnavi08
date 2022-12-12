e43ce3f8b4c1f62a163f618ca6e4cf42ae1ec10a
SaiVyshnavi08 <saivyshnavigudipalli@gmail.com>
2022-12-12 16:43:16 +0530

    Message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,14 +10,16 @@ public static class A {
 public static class B extends A{
 
 
- @Override
+      @Override
+
        public String meth(){
     return "Method is overridden in extended class B";
 }
 }
     public static void main(String[] args){
-     B b = new B();
-     b.meth();
+
+    A a = new A();
+    a.meth();
 
     B b1 = new B();
     b1.meth();
